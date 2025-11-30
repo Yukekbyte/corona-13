c829fe1f2cf5009c8ba5b6cae9d777495e8a054f
Yukekbyte <lowie.debois@gmail.com>
2025-11-30 09:15:11 +0100

    ris complete (besides accurate mis weights)

--- src/sampler.d/ris.c
+++ src/sampler.d/ris.c
@@ -34,12 +34,17 @@ typedef struct sampler_t {
   reservoir_t **reservoirs;
 } sampler_t;
 
+// returns random double between 0 and 1
+static double random_uniform() {
+  return ((double)rand()/(double)RAND_MAX);
+}
+
 // Updates reservoir with sample and weight.
 // Returns 1 if sample was replaced, 0 if not.
 static int update(reservoir_t *r, path_t *path, md_t weight, double c) {
   r->w_sum += weight;
   r->c += c;
-  if (((double)rand()/(double)RAND_MAX) < weight / r->w_sum) {
+  if (random_uniform() < weight / r->w_sum) {
     path_copy(r->path, path);
     return 1;
   }
@@ -60,18 +65,37 @@ static md_t p_hat(path_t *path) {
 }
 
 static void combine(reservoir_t *s, const reservoir_t *r) {
+  // can't combine reservoir with itself (happens when random_neighbor fails for example)
+  if(s == r) { printf("tried to combine reservoir with itself\n"); return; } 
+
+  // reservoirs can't be empty (although the path in the reservoir can still be the null sample (= uninitialized path))
+  if(s->c <= 0. && r->c <= 0.) { printf("tried to combine empty reservoirs"); return;}
 
   md_t w_r = 0.;
   md_t w_s = 0.;
+  md_t phat_r = p_hat(r->path);
+  md_t phat_s = p_hat(s->path);
+  //double rm = r->c * phat_r;
+  //double sm = s->c * phat_s;
+
+  // bias! how?
+  //double mis_r = (rm + sm) > 0. ? rm/(rm+sm) : 0.;
+  //double mis_s = (rm + sm) > 0. ? sm/(rm+sm) : 0.;
+  //printf("rm %f, sm %f, mis_r %f, mis_s %f\n", rm, sm, mis_r, mis_s);
+  double mis_r = r->c / (s->c + r->c);
+  double mis_s = s->c / (s->c + r->c);
+
 
   // if r has a valid sample (otherwise we call p_hat with an uninitialized_path which can give NaN)
-  if(r->w_sum > 0.)
-    w_r = 0.5 * p_hat(r->path) * r->W; // assume s and r for same pixel (starting vertex), then f(r.Y) * r.W = r.w_sum
-    //w_r = path_throughput(s->v[0], s->v[1], r->v[2]) * r->W; // f_q(r.Y) * estimate for 1/p_q'(r.Y)
+  if(r->w_sum > 0.) {
+    // TODO: construct path  [ s->v[0], s->v[1], r->v[2] ]
+    w_r = mis_r * phat_r * r->W; // assume s and r for same pixel (starting vertex), then f(r.Y) * r.W = r.w_sum
+    //w_r = mis_r * path_throughput(s->v[0], s->v[1], r->v[2]) * r->W; // f_q(r.Y) * estimate for 1/p_q'(r.Y)
+  }
   
   // if s has a valid sample
   if(s->w_sum > 0.)
-    w_s = 0.5 * p_hat(s->path) * s->W; // is equal to simply s->w_sum for now...
+    w_s = mis_s * phat_s * s->W;
 
   s->w_sum = w_s;
   update(s, r->path, w_r, r->c);
@@ -84,7 +108,7 @@ static void combine(reservoir_t *s, const reservoir_t *r) {
   // confidence capping
   if(s->c > 20.) s->c = 20.;
 }
-    
+
 sampler_t *sampler_init() {
   uint64_t i, j;
   uint64_t w = view_width(); // 1024 (standard)
@@ -139,9 +163,9 @@ static void ris(reservoir_t *r, const path_t *init_path) {
   r->w_sum = 0.;
   r->W = 0.;
 
-  int M = 8;
+  const int M = 8;
+  path_t path;
   for(int k = 0; k < M; k++) {
-    path_t path; // declaring this out of loop creates issues...
     path_copy(&path, init_path);
     
     // direct illumination, fails when hitpoint of init_path on envmap
@@ -192,9 +216,12 @@ void sampler_create_path(path_t *path)
     free(rris.path);
     return; 
   }
+
+  // lambda shifting
+  // lost code...
   
   // estimator f(r.Y) * r.W
-    // dirty quick fix: multiply by v[0].pdf * v[1].pdf, because r.W is an estimator of only v[2].pdf!
+    // quick fix: multiply by 1/v[0].pdf * 1/v[1].pdf, because r.W is an estimator of only 1/v[2].pdf!
   pointsampler_splat(r->path, md_2f(f(r->path) * r->W / (path->v[0].pdf * path->v[1].pdf)));
   
   path_copy(path, r->path);
